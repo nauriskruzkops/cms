2eb7a6db4f158987c2db885f8872938523051db9
Nauris Kruzkops <nauris.kruzkops@gmail.com>
2019-04-13 18:24:55 +0300

    Admin:  - Page edit - parent page list

--- src/Admin/Form/EventListener/PageListener.php
+++ src/Admin/Form/EventListener/PageListener.php
@@ -2,8 +2,11 @@
 
 namespace Admin\Form\EventListener;
 
+use Admin\Entity\Page;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -30,7 +33,44 @@ class PageListener implements EventSubscriberInterface
         return [
             FormEvents::PRE_SUBMIT   => 'preSubmit',
             FormEvents::POST_SUBMIT   => 'postSubmit',
+            FormEvents::PRE_SET_DATA => 'onPreSetData',
         ];
+    }
+
+
+    /**
+     * @param FormEvent $event
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function onPreSetData(FormEvent $event)
+    {
+        $form = $event->getForm();
+        /** @var Page $data */
+        $data = $event->getData();
+
+        //if ($form->getParent() instanceof Form) {
+        if ($data instanceof Page) {
+            $locale = $data->getLocale();
+            $form->remove('parent');
+            $form->add('parent', EntityType::class, [
+                'class' => Page::class,
+                'required' => false,
+                'query_builder' => function (EntityRepository $er) use ($locale) {
+                    return $er->createQueryBuilder('u')
+                        ->where('u.locale = :locale')->setParameter('locale', $locale)
+                        ->orderBy('u.left', 'ASC');
+                },
+                'choice_label' => function (Page $er) {
+                    return str_repeat('-', $er->getLevel()) .' '. $er->getTitle();
+                },
+                'label' => 'Parent page',
+                'empty_data' => '',
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ]);
+        }
+        //}
     }
 
     /**
