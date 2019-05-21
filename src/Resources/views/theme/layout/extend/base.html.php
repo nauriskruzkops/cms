9b05ae80c88c83fec5e1d87dcbb99ebdc6a34849
Nauris Kruzkops <nauris.kruzkops@gmail.com>
2019-05-21 10:56:49 +0300

    Site:  - Menu, responsive menu

--- src/Resources/views/theme/layout/extend/base.html.php
+++ src/Resources/views/theme/layout/extend/base.html.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Templating\PhpEngine;
  * @var GlobalVariables $app
  * @var PhpEngine $view
  */
+$assetVersion = '?'.substr(md5(date('YmdH')+1), 12); // Temp!
 
 ?><!doctype html>
 <html lang="<?= $view['locale']?>">
@@ -20,7 +21,7 @@ use Symfony\Bundle\FrameworkBundle\Templating\PhpEngine;
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="<?= $this->escape($view['settings']->value('site_description')) ?>">
     <link rel="icon" href="data:image/ico;base64,0">
-    <link href="<?= $view['assets']->getUrl('build/site.css') ?>" rel="stylesheet">
+    <link href="<?= $view['assets']->getUrl('build/site.css').$assetVersion ?>" rel="stylesheet">
 
 
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,8 +33,8 @@ use Symfony\Bundle\FrameworkBundle\Templating\PhpEngine;
     <?php $view['slots']->output('_content') ?>
 </div>
 
-<script src="<?= $view['assets']->getUrl('build/vendors~site.js') ?>"></script>
-<script src="<?= $view['assets']->getUrl('build/site.js') ?>"></script>
+<script src="<?= $view['assets']->getUrl('build/vendors~site.js').$assetVersion ?>"></script>
+<script src="<?= $view['assets']->getUrl('build/site.js').$assetVersion ?>"></script>
 
 </body>
 </html>
